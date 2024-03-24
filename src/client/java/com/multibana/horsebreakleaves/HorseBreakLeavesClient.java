df31642477389f1aa3615bbbe69e3fb02444d6d0
MultiBana <nirgeva98@gmail.com>
2024-03-24 14:58:51 +0200

    first commit

---
+++ src/client/java/com/multibana/horsebreakleaves/HorseBreakLeavesClient.java
@@ -0,0 +1,10 @@
+package com.multibana.horsebreakleaves;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HorseBreakLeavesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
